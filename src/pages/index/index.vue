69844e99884b5493ed5eeaa6842ba255d5d0f619
ycc <845837723@qq.com>
2024-08-02 15:20:15 +0800

    ci: 引入tdesign-mini

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,17 @@
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
-
+      <t-row>
+        <t-button theme="primary" size="small">填充按钮</t-button>
+      </t-row>
+      <t-row>
+        <t-radio :value="radio1" label="单选" default-checked />
+      </t-row>
+      <t-row>
+        <t-col span="8" t-class="dark">col-8</t-col>
+        <t-col span="8" t-class="light">col-8</t-col>
+        <t-col span="8" t-class="dark">col-8</t-col>
+      </t-row>
     </view>
   </view>
 </template>
@@ -11,6 +21,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello')
+const radio1 = ref(false)
 </script>
 
 <style>
